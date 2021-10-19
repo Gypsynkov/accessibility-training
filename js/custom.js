c9c3aee5b9b365f8f5c86b07a2f31af17aa09aa5
Mikhail_Tsyhankou <Mikhail_Tsyhankou@epam.com>
2021-10-19 22:04:29 +0300

    Basic JS

--- js/custom.js
+++ js/custom.js
@@ -1,14 +1,14 @@
-(function() {
+(function () {
   var burger = document.querySelector(".burger");
   var menu = document.querySelector("#" + burger.dataset.target);
-  burger.addEventListener("click", function() {
+  burger.addEventListener("click", function () {
     burger.classList.toggle("is-active");
     menu.classList.toggle("is-active");
   });
 })();
 
-document.querySelectorAll("#nav li").forEach(function(navEl) {
-  navEl.onclick = function() {
+document.querySelectorAll("#nav li").forEach(function (navEl) {
+  navEl.onclick = function () {
     toggleTab(this.id, this.dataset.target);
   };
 });
@@ -16,7 +16,7 @@ document.querySelectorAll("#nav li").forEach(function(navEl) {
 function toggleTab(selectedNav, targetId) {
   var navEls = document.querySelectorAll("#nav li");
 
-  navEls.forEach(function(navEl) {
+  navEls.forEach(function (navEl) {
     if (navEl.id == selectedNav) {
       navEl.classList.add("is-active");
     } else {
@@ -28,7 +28,7 @@ function toggleTab(selectedNav, targetId) {
 
   var tabs = document.querySelectorAll(".tab-pane");
 
-  tabs.forEach(function(tab) {
+  tabs.forEach(function (tab) {
     if (tab.id == targetId) {
       tab.style.display = "block";
     } else {
@@ -36,3 +36,104 @@ function toggleTab(selectedNav, targetId) {
     }
   });
 }
+
+(function () {
+  const keyCodes = {
+    end: 35,
+    home: 36,
+    left: 37,
+    right: 39,
+    enter: 13,
+    space: 32,
+  };
+  const arrow = {
+    37: -1,
+    39: 1,
+  };
+
+  function keydownEvent(event) {
+    const key = event.keyCode;
+
+    switch (key) {
+      case keyCodes.end:
+        event.preventDefault();
+        focusLastTab();
+        break;
+      case keyCodes.home:
+        event.preventDefault();
+        focusFirstTab();
+        break;
+    }
+  }
+  const tabsPane = document.querySelectorAll('[role="tab"]');
+
+  function addListeners(tab, index) {
+    tab.addEventListener("keydown", keydownEvent);
+    tab.addEventListener("keyup", keyupEvent);
+    tab.index = index;
+  }
+
+  function focusFirstTab() {
+    tabsPane[0].focus();
+  }
+
+  function focusLastTab() {
+    tabsPane[tabs.length - 1].focus();
+  }
+
+  tabsPane.forEach(addListeners);
+
+  function arrowControl(event) {
+    var currentKey = event.keyCode;
+    if (arrow[currentKey]) {
+      var target = event.target;
+      if (target.index !== undefined) {
+        if (tabsPane[target.index + arrow[currentKey]]) {
+          tabsPane[target.index + arrow[currentKey]].focus();
+        } else if (currentKey === keyCodes.left) {
+          focusLastTab();
+        } else if (currentKey === keyCodes.right) {
+          focusFirstTab();
+        }
+      }
+    }
+  }
+
+  const tabPanels = document.querySelectorAll('[role="tabpanel"]');
+  function deactiveTabs() {
+    tabsPane.forEach((item) => {
+      item.setAttribute("tabindex", "-1");
+      item.setAttribute("aria-selected", "false");
+    });
+
+    tabPanels.forEach((item) => {
+      if (item.classList.contains("is-active")) {
+        item.classList.remove("is-active");
+      }
+    });
+  }
+
+  function activeTab(item) {
+    deactiveTabs();
+    item.removeAttribute("tabindex");
+    item.setAttribute("aria-selected", "true");
+    var controls = item.getAttribute("aria-controls");
+    document.getElementById(controls).classList.add("is-active");
+  }
+
+  function keyupEvent(event) {
+    var key = event.keyCode;
+
+    switch (key) {
+      case keyCodes.left:
+      case keyCodes.right:
+        arrowControl(event);
+        break;
+
+      case keyCodes.enter:
+      case keyCodes.space:
+        activeTab(event.target);
+        break;
+    }
+  }
+})();
